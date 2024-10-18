bc213954a7ce55e8a788c3ac857f02117e56218f
jhohaly <jhohannes2017@gmail.com>
2024-10-17 19:07:35 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>OSMAR TUMBA NIÑAS</h1>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
+
